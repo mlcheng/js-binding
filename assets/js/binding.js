91ba75cbbba7349ee185c2ff4d13514a31e9743e
Michael Cheng <michael@michaelcheng.us>
2015-05-24 22:00:40 +0800

    Added some comments

--- assets/js/binding.js
+++ assets/js/binding.js
@@ -13,10 +13,25 @@
 
 var iqwerty = iqwerty || {};
 
+
 iqwerty.binding = (function() {
 	return {
+		/**
+		 * Bind an object's property to an array of HTML elements.
+		 * @param   obj  An object whose property should be bound
+		 * @param   prop The property of the object
+		 * @param   elem An array of HTML elements that should be bound to the data 
+		 * @return       Returns nothing
+		 */
 		bind: function(obj, prop, elem) {
 
+			/**
+			 * value is the HTML attribute where we can retrieve data from, e.g. "value" for input elements, or "innerHTML" for others
+			 * @type Array
+			 *
+			 * listener is the event that is called should changes occur
+			 * @type Array
+			 */
 			var value = [], listener = [];
 			for(var i=0;i<elem.length;i++) {
 				switch(elem[i].tagName.toLowerCase()) {
@@ -33,6 +48,9 @@ iqwerty.binding = (function() {
 				}
 			}
 
+			/*
+			Override the getter and setter methods to reflect on the given HTML elements
+			 */
 			Object.defineProperty(obj, prop, {
 				get: function() {
 					return elem[0][value[0]];
@@ -49,22 +67,21 @@ iqwerty.binding = (function() {
 
 
 
-
-			function listenerHelper(val) {
-				obj[prop] = val;
-			};
-
 			for(var i=0;i<elem.length;i++) {
 				if(listener[i] != null) {
 					var t_val = elem[i][value[i]];
 					elem[i].addEventListener(listener[i], function() {
-						obj[prop] = elem[0][value[0]];
+						obj[prop] = elem[0][value[0]]; //yeah something may be wrong here...
 					});
 				}
 			}
 			
 		},
 
+
+		/**
+		 * These are constants for the name of HTML elements.
+		 */
 		elements: {
 			ELEMENT_INPUT: "input",
 			ELEMENT_P: "p",
