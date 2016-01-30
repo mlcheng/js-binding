3fdd424c1c0a08b455cdb1af4da101e6be2f12f1
Michael Cheng <michael@michaelcheng.us>
2016-01-30 21:19:12 +0800

    Binding v2. Uses less memory and has much better binding performance.

--- binding.js
+++ binding.js
@@ -1,263 +1,256 @@
-/***********************************************
-
-  "binding.js"
-
-  Created by Michael Cheng on 05/29/2015 13:44
-            http://michaelcheng.us/
-            michael@michaelcheng.us
-            --All Rights Reserved--
-
-***********************************************/
-
 'use strict';
 
 var iqwerty = iqwerty || {};
 
 iqwerty.binding = (function() {
 
-	var OBJECT_EXP = '([^\.]+)\.([^\.]+)';
-	var ATTR_BINDING = 'data-iq-bind';
-	var HANDLEBAR_BINDING = 'data-iq-bind-scope';
+	var OBJ_ID = '$$id';
 
-	function Bind(obj, prop, elem) {
-		//First change the elem to array if needed
-		if(!(elem instanceof Array)) {
-			elem = [elem];
+	var VAR_EXP = '([a-zA-Z]+)';
+	var OBJ_EXP = VAR_EXP + '\.' + VAR_EXP;
+	var ATTRIBUTE_BINDING = 'data-iq-bind';
+	var HANDLEBAR_BINDING = 'data-iq-bind-scope';
+	var WRAPPER_IDEN = 'iq-bind';
+
+	function Bind(obj, prop, elems) {
+		//Turn elems into an array if not already
+		if(!(elems instanceof Array)) {
+			elems = [elems];
 		}
 
+		//Set an ID for the object first
+		Bind.prototype.setId(obj);
 
-		elem.forEach(function(el) {
-			/**
-			 * Set the initial value for each element
-			 * This is so that if the object property already
-			 * has a value, it is bound in the beginning
-			 */
-			Bind.prototype.setElementValue(el, obj[prop]);
+		//Update the bindings with the property, elements bound, and current value
+		Bind.prototype.updateBindings(obj[OBJ_ID], prop, elems, obj[prop]);
 
+		//Set initial bindings to update the view
+		Bind.prototype.updateViews(obj[OBJ_ID], prop);
 
-			//Allow the input event to change the data
+		//Add event listener for inputs to simulate two-way binding
+		elems.forEach(el => {
 			if(el.tagName.toLowerCase() === 'input') {
-				el.addEventListener('input', function() {
+				el.addEventListener('input', () => {
 					obj[prop] = el.value;
 				});
 			}
-
-
-			//Add the object to the list of bound elements
-			Bind.prototype.bound.push({ obj: obj, prop: prop, elem: el, '$$id': Bind.prototype.addObjectId(obj)});
-
-			//Set the object values for the getter
-			Bind.prototype.setObjects(obj.$$id, prop, obj[prop]);
 		});
 
-
-		/**
-		 * Override the getter and setter for the specified property
-		 */
+		//Set getters/setters
 		Object.defineProperty(obj, prop, {
-			get: function() {
-				var key = obj.$$id;
-				if(Bind.prototype.objects[key]) {
-					return Bind.prototype.objects[key][prop];
-				} else {
-					return undefined;
-				}
+			get: () => {
+				return Bind.prototype.Bindings[obj[OBJ_ID]][prop].value
 			},
-
-			set: function(val) {
-				elem.forEach(function(el) {
-					Bind.prototype.setter(obj, prop, el, val);
-				});
+			set: value => {
+				Bind.prototype.Bindings[obj[OBJ_ID]][prop].value = value;
+				Bind.prototype.updateViews(obj[OBJ_ID], prop);
 			},
-
 			configurable: true
 		});
 	}
 
 	/**
-	 * Find elements that are bound to the same object.
-	 * @param  {Object} obj  The object to find
-	 * @param  {String} prop The property of the object
-	 * @return {Array}       Returns an array of indices of Bind.prototype.bound that are bound together
+	 * Object properties and their respective bound elements
+	 *
+	 * e.g.
+	 * {
+	 * 	0: {
+	 * 		name: {
+	 * 			elems: [elems],
+	 * 		 	value: 'Michael'
+	 * 		}
+	 * 	}
+	 * }
+	 * @type {Object}
 	 */
-	Bind.prototype.findLink = function(obj, prop) {
-		return Bind.prototype.bound.filter(val => val.obj.$$id === obj.$$id && val.prop === prop);
-	}
+	Bind.prototype.Bindings = {};
+
+	Bind.prototype.addBinding = (obj) => {
+		var bindings = Bind.prototype.Bindings;
+		if(bindings[obj[OBJ_ID]]) return;
+
+		bindings[obj[OBJ_ID]] = {};
+		bindings = null;
+	};
 
 	/**
-	 * Set the value to the element
-	 * @param {Element} elem  The HTML element to set data to
-	 * @param {Object} value The value to set to the element
+	 * Update object bindings. Stores object and HTMLElement relationships
+	 * @param  {Number} id    The $$id of the object
+	 * @param  {String} prop  The property to be bound
+	 * @param  {Array} elems An array containing the HTMLElements to be bound
+	 * @param  {Object} value The value of the object
 	 */
-	Bind.prototype.setElementValue = function(elem, value) {
-		var v = 'innerHTML';
-		if(elem.tagName.toLowerCase() === 'input') {
-			v = 'value';
+	Bind.prototype.updateBindings = (id, prop, elems, value) => {
+		var bindings = Bind.prototype.Bindings;
+		if(!bindings[id]) {
+			bindings[id] = {};
 		}
-
-		//Emulate binding of data to the element
-		elem[v] = value;
-
-		elem = null;
-		v = null;
-	}
-
-	/**
-	 * A unique ID for objects
-	 * @type {Number}
-	 */
-	Bind.prototype.id = 0;
+		if(!bindings[id][prop]) {
+			bindings[id][prop] = {
+				elems: [],
+				value: value
+			};
+		}
+		//Add the bound elements
+		bindings[id][prop].elems.push(...elems);
+		bindings = null;
+	};
 
 	/**
-	 * Sets the unique ID to the objects
-	 * @param {Object} obj The object to add an ID to
-	 * @return {Number}    Returns the ID that was added
+	 * Update the views based on the Bindings relationships
+	 * @param  {Number} id   The object $$id
+	 * @param  {String} prop The property to bind
 	 */
-	Bind.prototype.addObjectId = function(obj) {
-		if(obj.$$id !== undefined) return obj.$$id;
-		obj.$$id = Bind.prototype.id;
-		return Bind.prototype.id++;
-	}
+	Bind.prototype.updateViews = (id, prop) => {
+		var elems = Bind.prototype.Bindings[id][prop].elems;
 
-	/**
-	 * The setter for Bind
-	 * @param {Object} obj  The object of the data to bind
-	 * @param {Object} prop The property of the object to bind
-	 * @param {Element} elem The HTML element to bind data to
-	 * @param {Object} val  The new value of the object property
-	 */
-	Bind.prototype.setter = function(obj, prop, elem, val) {
-		var links = Bind.prototype.findLink(obj, prop);
-		links.forEach(function(link) {
-			Bind.prototype.setElementValue(link.elem, val);
+		var _value;
+		elems.forEach(el => {
+			if(el.tagName.toLowerCase() === 'input') {
+				_value = 'value';
+			} else {
+				_value = 'innerHTML';
+			}
+			el[_value] = Bind.prototype.Bindings[id][prop].value;
 		});
 
-		Bind.prototype.setObjects(obj.$$id, prop, val);
-
-		links = null;
-	}
-
-	/**
-	 * Set the object values to memory; used for the object getter
-	 * @param {Number} key  The ID of the object
-	 * @param {String} prop The property to add
-	 * @param {Object} val  The value of the property
-	 */
-	Bind.prototype.setObjects = function(key, prop, val) {
-		Bind.prototype.objects[key] = Bind.prototype.objects[key] || {};
-		Bind.prototype.objects[key][prop] = val;
-	}
+		elems = null;
+		_value = null;
+	};
 
 	/**
-	 * An array that stores information about which objects are bound together
-	 * @type {Array}
+	 * A unique ID to be used for binding
+	 * @type {Number}
 	 */
-	Bind.prototype.bound = [];
-
-	/**
-	 * An object that stores the data that is bound
-	 * This is used for the getters
-	 * @type {Object}
-	 */
-	Bind.prototype.objects = {};
-
-	/**
-	 * Stores the model for data binding attributes
-	 * @type {Object}
-	 */
-	Bind.prototype.model = {};
-
-	/**
-	 * Attempts data binding all of the elements
-	 * that have the iqBind attribute
-	 * NB: iqwerty.binding.Model() must be used with this
-	 */
-	function BindAttributes() {
-		setTimeout(function() {
-			var elem = document.querySelectorAll('[' + ATTR_BINDING + ']');
-			if(elem.length === 0) return;
-
-			[].slice.call(elem).forEach(function(el) {
-				var obj = el.dataset.iqBind;
-				var res = new RegExp(OBJECT_EXP).exec(obj);
-				if((!res) || !Bind.prototype.model[res[1]]) return;
-
-
-				Bind(Bind.prototype.model[res[1]], res[2], el);
+	Bind.prototype._id = 0;
+	Bind.prototype.getId = () => Bind.prototype._id++;
+	Bind.prototype.setId = obj => {
+		if(obj[OBJ_ID] == null) { //Or undefined
+			Object.defineProperty(obj, OBJ_ID, {
+				value: Bind.prototype.getId()
 			});
+		}
+	};
+
+	/**
+	 * Binding for elements bound using data-iq-bind attribute
+	 * @param {Object} models The binding models from the user, i.e. objects to bind
+	 */
+	function BindAttributes(models) {
+		setTimeout(function() {
+			var elems = document.querySelectorAll(`[${ATTRIBUTE_BINDING}]`);
+			if(elems.length === 0) return;
+
+			[].slice.call(elems).forEach(el => {
+				var literal = el.dataset.iqBind;
+				literal = new RegExp(OBJ_EXP).exec(literal);
+				if(!literal) return;
+				var objName = literal[1];
+				var obj = models[objName];
+				Bind(obj, literal[2], el);
+
+				literal = null;
+				objName = null;
+				obj = null;
+			});
+
+			elems = null;
 		}, 0);
 	}
 
 	/**
-	 * Attempts data binding of elements with handlebars
-	 * NB: iqwerty.binding.Model() must be used with this
+	 * Binding for elements using handlebars
+	 * @param {Object} models The binding models from the user, i.e. objects to bind
 	 */
-	function BindHandlebars() {
+	function BindHandlebars(models) {
 		setTimeout(function() {
-			var elem = document.querySelectorAll('[' + HANDLEBAR_BINDING + ']');
-			if(elem.length === 0) return;
+			var elems = document.querySelectorAll(`[${HANDLEBAR_BINDING}]`);
+			if(elems.length === 0) return;
 
-			var IDENTIFIER = 'iq-bind';
-			[].slice.call(elem).forEach(function(el) {
+
+			[].slice.call(elems).forEach(el => {
+				var scoped = el.dataset.iqBindScope;
+				var exp = '{ *?' + (scoped ? VAR_EXP : OBJ_EXP) + ' *?}';
+				exp = new RegExp(exp, 'g');
+
+
 				var handlebars = [];
 
-				var exp = '{ *?' + OBJECT_EXP + ' *?}';
-				exp = new RegExp(exp, 'g');
 				var match;
 				do {
 					match = exp.exec(el.innerHTML);
-					if(match) {
-						handlebars.push({
-							template: match[0],
-							obj: match[1],
-							prop: match[2],
-							id: Bind.prototype.id++
-						});
-					}
+					if(!match) continue;
+					handlebars.push({
+						matched: match[0],
+						obj: scoped || match[1],
+						prop: scoped ? match[1] : match[2],
+						id: WRAPPER_IDEN + '-' + Bind.prototype.getId()
+					});
 				} while(match);
+				match = null;
 
-				handlebars.forEach(function(handlebar) {
+				handlebars.forEach(handlebar => {
 					var _bind = document.createElement('span');
-					_bind.classList.add(IDENTIFIER);
-					_bind.id = IDENTIFIER + '-' + handlebar.id;
-					el.innerHTML = el.innerHTML.replace(handlebar.template, _bind.outerHTML);
+					_bind.classList.add(WRAPPER_IDEN);
+					_bind.id = handlebar.id;
+					el.innerHTML = el.innerHTML.replace(handlebar.matched, _bind.outerHTML);
+					_bind = null;
 				});
 
-				handlebars.forEach(function(handlebar) {
-					Bind(Bind.prototype.model[handlebar.obj], handlebar.prop, document.getElementById(IDENTIFIER + '-' + handlebar.id));
-				})
+				//This must be separate from the above
+				//If it isn't separated, then the .innerHTML will overwrite the element from before and the reference to the node will be gone.
+				handlebars.forEach(handlebar => {
+					var obj = models[handlebar.obj];
+					var el = document.getElementById(handlebar.id);
+					Bind(obj, handlebar.prop, el);
+
+					//Stop id pollution
+					el.removeAttribute('id');
+					obj = null;
+					el = null;
+				});
+
+				scoped = null;
+				exp = null;
+				handlebars = null;
 			});
+
+			elems = null;
 		}, 0);
 	}
 
 	/**
-	 * Parse the page for attribute or handlebar bindings
+	 * Set the binding model
+	 * Used for attribute and handlebar binding
+	 * @param {Object} models A binding object, e.g.
+	 * {
+	 * 	person: personObject,
+	 * 	animal: animalObject
+	 * }
+	 * 
+	 * Then 'person' and 'animal' can be bound
+	 */
+	function Model(models) {
+		Object.keys(models).forEach(model => {
+			Bind.prototype.setId(models[model]);
+			Bind.prototype.addBinding(models[model]);
+		});
+		BindAttributes(models);
+		BindHandlebars(models);
+	}
+
+	/**
+	 * Parse the page for attributes and handlebars.
+	 * I'm not sure if this should be exposed, let alone needed. Removed from exporting for now.
 	 */
 	function ParsePage() {
 		BindAttributes();
 		BindHandlebars();
 	}
 
-	/**
-	 * Adds the data binding object to the model
-	 * @param {Object} models An object containing the data to bind.
-	 */
-	function Model(models) {
-		Object.keys(models).forEach(function(model) {
-			Bind.prototype.model[model] = models[model];
-		});
-	}
-
-
 	return {
 		Bind: Bind,
-		BindAttributes: BindAttributes,
-		BindHandlebars: BindHandlebars,
-		ParsePage: ParsePage,
 		Model: Model
+		//ParsePage: ParsePage
 	};
 })();
-
-
-//This binds elements with the iq-bind attribute with its data
-document.addEventListener('DOMContentLoaded', iqwerty.binding.ParsePage);
